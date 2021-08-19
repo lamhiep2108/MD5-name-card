3b5c9af03f59d468637efa1a915a0f2d5bff02e3
lamhiep2108 <hiepnothip@gmail.com>
2021-08-19 09:24:09 +0700

    l

---
+++ src/app/name-card/name-card.component.ts
@@ -0,0 +1,21 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-name-card',
+  templateUrl: './name-card.component.html',
+  styleUrls: ['./name-card.component.scss']
+})
+export class NameCardComponent implements OnInit {
+  // @ts-ignore
+  @Input() cardName: string;
+  // @ts-ignore
+  @Input() email: string;
+  // @ts-ignore
+  @Input() phone: string;
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
